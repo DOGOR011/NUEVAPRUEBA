3047a679a18d3155deff1d36c644c925b1588471
DOGOR011 <36340889+DOGOR011@users.noreply.github.com>
2018-02-16 21:07:12 -0500

    cambio1
    probando el primer  cambio

---
+++ Untitled2.c
@@ -0,0 +1,16 @@
+/*leer y escribrir un caracter*/
+
+#include <stdio.h>
+
+main() {
+	char car;
+		printf("ESSCRIBA UN CARACTER: \n");
+		car=getchar();
+		printf("\nEl caracter introducido es %c.\n, car ");
+		
+	
+
+
+
+
+}
